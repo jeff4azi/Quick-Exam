fcf8eb487b9a3e3c8f87629462b51e458cc62358
Jeffrey <164729708+jeff4azi@users.noreply.github.com>
2026-02-07 21:19:24 +0100

    Update BannerAd.jsx

--- src/components/BannerAd.jsx
+++ src/components/BannerAd.jsx
@@ -19,7 +19,7 @@ const AD_DATABASE = [
     type: "video",
     url: "https://res.cloudinary.com/dxdnhc1hm/video/upload/v1770495366/YouCut_20260207_210718090_zfu7lv.mp4",
     duration: 10,
-    whatsapp: "2340000000000",
+    whatsapp: "2349069306883",
   },
 ];
 
